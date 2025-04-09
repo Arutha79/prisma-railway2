c408ecca90a0801622b268513db8e17769939644
Arutha79 <guillaume.apide@gmail.com>
2025-04-09 09:33:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.send("🎯 Prisma Railway is running"));
+app.get("/", (req, res) => {
+  res.send("🎯 Prisma Railway is running !");
+});
 
-app.listen(PORT, () => console.log("✅ Prisma en ligne sur Railway"));
+app.listen(PORT, () => {
+  console.log(`✅ Serveur en ligne sur le port ${PORT}`);
+});
